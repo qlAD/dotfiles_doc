f71d83f3457d4e34fcf14ffadd3dfea42d7502a5
qlAD <3394543809@qq.com>
2022-07-29 00:47:38 +0800

    Update Logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -4,7 +4,7 @@ import styles from './logo.module.css'
 export default function Logo() {
   return (
     <>
-      <Image src="/icons/Arch-linux-logo.png" width="36" />
+      <Image src="/icons/Arch-linux-logo.png" width="36" height="36" />
       <div className={styles.logo}>
         <span className={styles.logo__title}>qlAD&s Arch Linux dotfiles</span>
         <span className={styles.logo__subtitle}>qlad&s Arch Linux configuration file</span>
